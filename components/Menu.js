737f1897c74f447901b881b82d76a612cba90f24
Rosie-Brigham <rosiebrigham@gmail.com>
2016-07-20 15:05:55 +0100

    adds functionality to menu

--- components/Menu.js
+++ components/Menu.js
@@ -3,38 +3,32 @@ import React, { Component } from 'react'
 class Menu extends Component {
   constructor(props) {
     super(props)
-    console.log(props)
-  }
-
-  thing() {
-    this.foo = 'foo'
-    this.props.enter()
   }
 
   render() {
     return(
-      <p id="menu">
-        <section onClick={this.foo}>
-          <div className="logo-placholder"></div>
+      <div id="menu">
+        <section id="Danger" onClick={this.props.changeView}>
+          <div id="Danger" className="logo-placholder"></div>
           In Danger
         </section>
-        <section>
-          <div className="logo-placholder"></div>
-          Getting Help
+        <section id="Leave" onClick={this.props.changeView}>
+          <div id="Leave" className="logo-placholder"></div>
+          Find shelter
         </section>
-        <section>
-          <div className="logo-placholder"></div>
+        <section id="Legal" onClick={this.props.changeView}>
+          <div id="Legal" className="logo-placholder"></div>
           Build a legal case
         </section>
-        <section>
-          <div className="logo-placholder"></div>
+        <section id="Friend" onClick={this.props.changeView}>
+          <div id="Friend" className="logo-placholder"></div>
           A friend is in danger
         </section>
-        <section id="last">
-          <div className="logo-placholder"></div>
+        <section id="Leave" onClick={this.props.changeView}>
+          <div id="Leave" className="logo-placholder"></div>
           Online Safety
         </section>
-      </p>
+      </div>
       )
   }
 }
